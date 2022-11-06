4a8f623d5086fcd084ad72403f9628b69bbb6883
lukyrasocha <rasocha.lukas@gmail.com>
2022-11-06 22:34:10 +0100

    Final design touches

--- src/real_time_plotting.py
+++ src/real_time_plotting.py
@@ -1,10 +1,19 @@
 import datetime as dt
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import matplotlib as mpl
 
 # Create figure for plotting
-fig = plt.figure()
+COLOR = '#FFFFFF'
+mpl.rcParams['text.color'] = COLOR
+mpl.rcParams['axes.labelcolor'] = COLOR
+mpl.rcParams['xtick.color'] = COLOR
+mpl.rcParams['ytick.color'] = COLOR
+
+fig = plt.figure(figsize = (10,5),tight_layout=True, facecolor='#262626')
 ax = fig.add_subplot(1, 1, 1)
+ax.set_facecolor("#262626")
+
 xs = []
 ys = []
 y_avg = []
@@ -27,14 +36,15 @@ def animate(i, xs, ys, y_avg):
 
     # Draw x and y lists
     ax.clear()
-    ax.plot(xs, ys, label='mV')
-    ax.plot(xs, y_avg, label ='Moving average [mV]')
+    ax.plot(xs, ys, label='mV', marker='o', color='#08F7FE')
+    ax.plot(xs, y_avg, label ='Moving average [mV]', color='#FE53BB')
 
     # Format plot
     plt.xticks(rotation=45, ha='right')
     plt.subplots_adjust(bottom=0.30)
     plt.title('RoGlove input measurements [mV]')
-    plt.legend(loc='upper right')
+    plt.legend(loc='upper right', framealpha=0)
+    plt.grid(color='#454545', linestyle='-', linewidth=0.3)
     plt.ylabel('mV')
 
 # Set up plot to call animate() function periodically
